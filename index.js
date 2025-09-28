a029347fb57d8b89d8792ab24deded1cc1a5c39c
TranQuangKhanh <quangkhanh1029@gmail.com>
2025-09-28 22:31:10 +0700

    Change vercel

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World from Vercel!");
+  res.send("Hello World");
 });
 module.exports = app;
