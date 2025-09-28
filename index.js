bc88651aac592314a286f6ba765a1774f217d530
TranQuangKhanh <quangkhanh1029@gmail.com>
2025-09-28 22:26:29 +0700

    Fix vercel

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World from Vercel!");
 });
-
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:2025");
-});
+module.exports = app;
